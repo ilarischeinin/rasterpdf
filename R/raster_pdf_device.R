8baa36b8b7c75fe5d79ef0006637d43947c01fff
Ilari Scheinin <ilari.scheinin@gmail.com>
2019-08-08 22:29:33 +0300

    Rename closure's cache

--- R/raster_pdf_device.R
+++ R/raster_pdf_device.R
@@ -1,17 +1,17 @@
 raster_pdf_device_closure <- function() {
-  storage <- list()
-  
+  raster_pdf_devices <- list()
+
   function(which = NULL, device = NULL) {
     if (!is.null(which)) {
       stopifnot(is.numeric(which))
       which <- as.integer(which)
-      if (which <= length(storage)) {
-        return(storage[[which]])
+      if (which <= length(raster_pdf_devices)) {
+        return(raster_pdf_devices[[which]])
       } else {
         return(NULL)
       }
     } else {
-      storage[[grDevices::dev.cur()]] <<- device
+      raster_pdf_devices[[grDevices::dev.cur()]] <<- device
       return(invisible())
     }
   }
