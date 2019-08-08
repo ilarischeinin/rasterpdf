257657f5d0343a7edca84b5cba97574c393e3fef
Ilari Scheinin <ilari.scheinin@gmail.com>
2019-08-08 20:57:06 +0300

    Fix indent

--- R/raster_pdf_device.R
+++ R/raster_pdf_device.R
@@ -1,21 +1,20 @@
-raster_pdf_device_closure <-
-  function() {
-    storage <- list()
-
-    function(which = NULL, device = NULL) {
-      if (!is.null(which)) {
-        stopifnot(is.numeric(which))
-        which <- as.integer(which)
-        if (which <= length(storage)) {
-          return(storage[[which]])
-        } else {
-          return(NULL)
-        }
+raster_pdf_device_closure <- function() {
+  storage <- list()
+  
+  function(which = NULL, device = NULL) {
+    if (!is.null(which)) {
+      stopifnot(is.numeric(which))
+      which <- as.integer(which)
+      if (which <= length(storage)) {
+        return(storage[[which]])
       } else {
-        storage[[grDevices::dev.cur()]] <<- device
-        return(invisible())
+        return(NULL)
       }
+    } else {
+      storage[[grDevices::dev.cur()]] <<- device
+      return(invisible())
     }
   }
+}
 
 raster_pdf_device <- raster_pdf_device_closure()
