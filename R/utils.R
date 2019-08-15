e8332ebfe77f9682e65d51b91eb5228b8ecba276
Ilari Scheinin <ilari.scheinin@gmail.com>
2019-08-15 21:22:29 +0300

    Move all internal functions to one file

--- R/utils.R
+++ R/utils.R
@@ -11,3 +11,24 @@ convert_to_inches <- function(x, units, res) {
     NA_real_
   }
 }
+
+raster_pdf_device_closure <- function() {
+  raster_pdf_devices <- list()
+
+  function(which = NULL, device = NULL) {
+    if (!is.null(which)) {
+      stopifnot(is.numeric(which))
+      which <- as.integer(which)
+      if (which <= length(raster_pdf_devices)) {
+        return(raster_pdf_devices[[which]])
+      } else {
+        return(NULL)
+      }
+    } else {
+      raster_pdf_devices[[grDevices::dev.cur()]] <<- device
+      return(invisible())
+    }
+  }
+}
+
+raster_pdf_device <- raster_pdf_device_closure()
